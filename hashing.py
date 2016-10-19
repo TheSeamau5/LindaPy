6ecbb10fd2740ee5dfc1bed89a0f822166392834
Hassan Hayat <hassan.hayat7@gmail.com>
2016-10-19 08:08:11 -0700

    Fix issue with removing item where removing item caused unbalances

--- hashing.py
+++ hashing.py
@@ -1,4 +1,9 @@
-def generate_hasher(n = 1024):
+# Consistent Hashing Implementation
+
+
+# Function to generate a hash table generator
+# n : number of slots (default = 1024)
+def generate_consistent_hasher(n=1024):
     # Generate a table
     def generate_table(addresses):
         m = len(addresses)
@@ -8,33 +13,22 @@ def generate_hasher(n = 1024):
     return generate_table
 
 
+# Consistent hash function
+consistent_hash = generate_consistent_hasher(1024)
+
+
+# Find the indices that have changed between two lists
 # Assuming l1 and l2 have the same length
 def diff_indices(l1, l2):
-    return [i for i in range(len(l1)) if not l1[i] == l2[i]]
+    return [i for i in range(min(len(l1), len(l2))) if not l1[i] == l2[i]]
 
 
-def split_regions(table):
-    result = []
-    temp = []
-    prev = None
-    for item in table:
-        if prev is None or prev == item:
-            temp.append(item)
-
-        else:
-            result.append(temp)
-            temp = [item]
-
-        prev = item
-
-    result.append(temp)
-    return result
-
-
+# Get the number of different items in a table
 def _get_num_items(table):
     return len(set(table))
 
 
+# Convert a table to a dictionary
 def convert_to_dict(table):
     d = {}
     for i in range(len(table)):
@@ -45,16 +39,8 @@ def convert_to_dict(table):
             d[item] = [i]
     return d
 
-# import pytest
-# from pytest import list_of
-#
-# @pytest.mark.randomize(table=list_of(int), ncalls=1000)
-# def test_convert_table(table):
-#     d = convert_to_dict(table)
-#     t = convert_to_list(d)
-#     assert(t == table)
 
-
+# Convert a dictionary to a table
 def convert_to_list(d):
     l = []
     for item, indices in d.items():
@@ -64,15 +50,7 @@ def convert_to_list(d):
     return [x[1] for x in sorted_l]
 
 
-# def pick_indices_from_dict(ratio, d):
-#     new_indices = []
-#     for item, indices in d.items():
-#         threshold = round((1 - ratio) * len(indices))
-#         new_indices.extend(indices[threshold:])
-#     return new_indices
-
-
-def get_indices_for_new_item(table):
+def _get_indices_for_new_item(table):
     d = convert_to_dict(table)
     num_items = _get_num_items(table)
     total = len(table)
@@ -91,24 +69,40 @@ def get_indices_for_new_item(table):
     return indices
 
 
-def _generate_chunks(l, n):
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
+def split_into_chunks(l, k):
+    n = len(l)
+    if n == 0 or k <= 1:
+        return l
+
+    chunks = []
+    current_chunk = []
+    current_chunk_index = 0
+    for i in range(n):
+        f = i / n
+        j = current_chunk_index + 1
+        g = j / k
+        if f >= g:
+            current_chunk_index += 1
+            chunks.append(current_chunk)
+            current_chunk = []
+        current_chunk.append(l[i])
+
+    if len(current_chunk):
+        chunks.append(current_chunk)
+
+    return chunks
 
 
-def split_into_chunks(l, n):
-    return list(_generate_chunks(l, n))
-
-
-def get_index_dict_for_remove_item(item, d):
-    print('Dict: {0}'.format(d))
+def _get_index_dict_for_remove_item(item, d):
     result = {}
-    keys = [x for x in list(d.keys()) if not x == item]
-    print('Keys: {0}'.format(keys))
+    sorted_kv_list = sorted([(k, v) for k, v in d.items() if not k == item], key=lambda kv: len(kv[1]))
+    keys = [k for k, v in sorted_kv_list if not k == item]
+
     num_keys = len(keys)
     indices = d[item]
-    chunks = split_into_chunks(indices, num_keys)
-    print('Chunks: {0}'.format(chunks))
+    num_chunks = max(1, min(num_keys, len(indices) - 1))
+    chunks = split_into_chunks(indices, num_chunks)
+
     for i in range(len(chunks)):
         result[keys[i]] = chunks[i]
 
@@ -140,11 +134,19 @@ def generate_replica_table(table):
     return [(x['index'], x['replica']) for x in descriptive_table]
 
 
+def get_replica(index, table):
+    try:
+        result = next(x[1] for x in generate_replica_table(table) if x[0] == index)
+        return result
+    except:
+        return None
+
+
 def remove_item(item, table):
     new_table = table
     if item in table:
         d = convert_to_dict(table)
-        index_dict = get_index_dict_for_remove_item(item, d)
+        index_dict = _get_index_dict_for_remove_item(item, d)
         del d[item]
         for item, indices in index_dict.items():
             if item in d:
@@ -158,7 +160,7 @@ def remove_item(item, table):
 def add_item(item, table):
     new_table = list(table)
     if not item in table:
-        new_indices = get_indices_for_new_item(table)
+        new_indices = _get_indices_for_new_item(table)
         for i in new_indices:
             new_table[i] = item
     return new_table
